dc046aea4faa1621f4e0bbe55923e8a99444502d
Paco Torres <paco.tcast@gmail.com>
2021-11-27 12:21:48 -0600

    config/add-swagger-and-versioning

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,10 +1,20 @@
 import { Get, Post, Put, Delete, Body, Param, Controller, Query } from '@nestjs/common';
+import {
+    ApiOperation,
+    ApiTags,
+    ApiHeader
+  } from '@nestjs/swagger';
 import { CreateGameDto } from './dtos/create-game.dto';
 import { UpdateGameDto } from './dtos/update-game.dto';
 import { GameProducerService } from './game.producer.service';
 import { GameService } from './game.service';
 
-@Controller('game')
+@ApiTags('game')
+@ApiHeader({ name: 'API-VERSION', description: 'Resource version'})
+@Controller({
+    path: 'game',
+    version: '1'
+})
 export class GameController {
     constructor(
         private gameService: GameService,
@@ -12,36 +22,43 @@ export class GameController {
     ) {}
 
     @Get()
+    @ApiOperation({ description: 'List all games' })
     list() {
         return this.gameService.list();
     }
 
     @Get('/:id')
+    @ApiOperation({ description: 'Retrieve a single game by id' })
     findById(@Param('id') id: number) {
         return this.gameService.findById(id);
     }
 
     @Get('/:id/publisher')
+    @ApiOperation({ description: 'Retrieve publisher data for a given game id' })
     getPublisherDataByGameId(@Param('id') id: number) {
         return this.gameService.retrievePublisherDataByGameId(id);
     }
 
     @Post()
+    @ApiOperation({ description: 'Create a game' })
     create(@Body() body: CreateGameDto) {
         return this.gameService.create(body);
     }
 
-    @Post('/price-adjustment-task')
+    @Post('/game-adjustment-task')
+    @ApiOperation({ description: 'Start task for updating/deleting games based on its age' })
     invokePriceAdjustmentTask() {
         return this.gameProducerService.timeGameAdjustments();
     }
 
     @Put('/:id')
+    @ApiOperation({ description: 'Update game information' })
     update(@Param('id') id: number, @Body() body: UpdateGameDto) {
         return this.gameService.update(id, body);
     }
 
     @Delete('/:id')
+    @ApiOperation({ description: 'Delete a game' })
     delete(@Param('id') id: number) {
         return this.gameService.delete(id);
     }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +13,21 @@ async function bootstrap() {
       whitelist: true,
     })
   );
+  app.enableVersioning({
+    type: VersioningType.HEADER,
+    header: 'API-VERSION',
+  })
+
+  const config = new DocumentBuilder()
+    .setTitle('Games API')
+    .setDescription('Games API')
+    .setVersion('1.0')
+    .addTag('game')
+    .addTag('publisher')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
